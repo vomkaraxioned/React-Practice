02a92b3bd3d7d23451442d97216472c79215da1c
vomkaraxioned <omkarv@axioned.com>
2022-11-29 18:58:33 +0530

    added all hook

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 // import Usestate from "./hooks/useState.js";
 // import Useeffect from "./hooks/useeffect.js";
 // import Usecontext from"./hooks/usecontext.js";
-import Usememo from "./hooks/usememo.js";
+// import Usememo from "./hooks/usememo.js";
+import Usecallback from "./hooks/callback.js";
 
 function App(props) {
   return (
-    <Usememo />
+    <Usecallback />
   );
 }
 
