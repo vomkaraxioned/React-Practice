5972b0b283d4e0b21a4b3a9aa466646be489c623
vomkaraxioned <omkarv@axioned.com>
2022-11-30 18:57:12 +0530

    styled components

---
+++ src/components/FetchedData.js
@@ -0,0 +1,26 @@
+ import Api from "./axios.js";
+ import {useState,useEffect} from "react";
+
+ const FetchedData = ()=>{
+
+  const [data,setData]=useState([]),
+  [error,setError] = useState(null),
+  getData = ()=>{
+    Api.get("/posts").then(res=>setData(res.data)).catch(err=>setError(err.message));
+  };
+
+  useEffect(getData,[]);
+
+    if(error) {
+      return (<h1>{error}</h1>);
+    }else {
+      return(<ul>
+        {data.map(post=>{
+          return <li key={post.id}>{post.title}</li>
+        })}
+      </ul>);
+    }
+ };
+
+
+ export default FetchedData;
---
+++ src/pages/App.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import FetchedData from "../components/FetchedData.js";
+
+
+const App = ()=>{
+  return(
+    <FetchedData/>
+  );
+};
+
+export default App;
