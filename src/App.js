22991f6f0ac4600e11a244bf289ea4b05150d8dc
vomkaraxioned <omkarv@axioned.com>
2022-12-02 13:04:29 +0530

    tailwind example

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import './App.css';
-import FirstComponent,{FuncComponent as Fn,FuncEffect as Fe} from './components/component.js';
-import ContextNode from './components/context.js';
-import reducerFunc from "./components/reducer.js";
-import ReducerFunc from './components/reducer.js';
-import Index from './components/index.js';
 
 function App(props) {
   return (
-//   <FirstComponent name={props.name} age={props.age}/>
-//   <Fn name={props.name} age={props.age} count="10"/>
-//   <Func name={props.name} age={props.age} count="10"/>
-//  <ContextNode />
-//     <ReducerFunc/>
-    <Index />
+   <h1 className=" py-5 mx-auto capitalize bg-black text-white text-2xl flex justify-center  xl:w-1/2 xl:hover:bg-violet-900">new header</h1>
   );
 }
 
